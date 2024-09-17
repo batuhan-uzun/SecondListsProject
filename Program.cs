c8095c1d23a5507fb573d2a7174379412ed26f92
batuhan-uzun <batuhanuzun2356@gmail.com>
2024-09-18 02:23:53 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿List<string> coffeeName = new List<string>(); // Created List of type String
+Console.WriteLine("Please enter 5 coffee names");
+for (int i = 0; i < 5; i++) // For loop with 5 coffee names requested from the user
+{
+    coffeeName.Add(Console.ReadLine());
+}
+
+Console.WriteLine("Entered coffee names:");
+foreach (string names in coffeeName) // Names in coffees were printed on the screen with a forach
+{
+    Console.WriteLine(names);
+}
